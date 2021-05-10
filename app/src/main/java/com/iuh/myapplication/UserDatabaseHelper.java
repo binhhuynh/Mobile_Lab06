ddd94fcba8bab1dc4ead6f408c97476d9c3dbcbe
Thanh Bình Huỳnh <huynhthanhbinh290@gmail.com>
2021-05-10 10:46:58 +0700

    Initial commit

---
+++ app/src/main/java/com/iuh/myapplication/UserDatabaseHelper.java
@@ -0,0 +1,36 @@
+package com.iuh.myapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "user";
+    private static final int DB_VERSION = 1;
+
+    UserDatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        updateMyDatabase(db, 0, DB_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("CREATE TABLE USER (_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "NAME TEXT);");
+    }
+
+    private void insertUser(SQLiteDatabase db, String name) {
+        ContentValues userValues = new ContentValues();
+        userValues.put("NAME", name);
+        db.insert("USER", null, userValues);
+    }
+}
